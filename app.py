22fdaccb525f14464de3a38b485fb968f945fe1b
Pickle <chaopangchiu@gmail.com>
2024-12-17 16:41:01 +0800

    Add app.py and requirements

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, abort
+from dotenv import load_dotenv
+import os
+import re
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
+app = Flask(__name__)
+
+# Load .env file
+load_dotenv()
+
+channel_access_token = os.getenv("CHANNEL_ACCESS_TOKEN")
+channel_secret = os.getenv("CHANNEL_SECRET")
+
+configuration = Configuration(access_token=channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+keyword = "哈囉"
+allowed_chars = r".*"
+pattern = allowed_chars.join(keyword)
+regex = rf"{pattern}"
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+
+    if re.search(regex, event.message.text):
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text="你哈囉了！請勿哈囉！")]
+            )
+        )
+    else:
+        pass
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
