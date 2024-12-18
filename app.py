5ba1cdbfc0b5d98c23ee266d3cba42ae77c60eaa
Pickle <chaopangchiu@gmail.com>
2024-12-18 17:35:26 +0800

    modify user_id

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def handle_message(event):
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=event.source.userId)]
+                messages=[TextMessage(text=event.source.user_id)]
             )
         )
 
